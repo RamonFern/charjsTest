77666e933d52e5be1c9c75b6cc602ee20398f168
ramon <ramon@petize.com.br>
2023-08-15 23:19:16 -0300

    iniciando módulo de pesquisar veículo

--- src/app/notificacoes/notificacoes.module.ts
+++ src/app/notificacoes/notificacoes.module.ts
@@ -4,15 +4,20 @@ import { CommonModule } from "@angular/common";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { PesquisarComponent } from "./pesquisar/pesquisar.component";
+import { MatButtonModule } from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
-    NotificarComponent
+    NotificarComponent,
+    PesquisarComponent
   ],
   imports: [
     CommonModule,
     MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
     MatInputModule,
     MatIconModule
   ]
