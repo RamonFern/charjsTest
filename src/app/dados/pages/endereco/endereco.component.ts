1e0df1a408e9327e97f7f443804f3e8de061ba64
RamonFern <ramonFernandes9@gmail.com>
2026-04-12 12:24:37 -0300

    nova tent

--- src/app/dados/pages/endereco/endereco.component.ts
+++ src/app/dados/pages/endereco/endereco.component.ts
@@ -87,10 +87,11 @@ export class EnderecoComponent implements OnInit {
     this.relatorioService.buscarTodos()
         .pipe((take(1)))
         .subscribe((rel) => {
-          this.relatorios = rel.sort((x, y) => {
-            // return new Date(x.datadorelatorio).getTime() - new Date(y.datadorelatorio).getTime();
-              return new Date(y.datadorelatorio).getTime() - new Date(x.datadorelatorio).getTime();
-          });
+          // this.relatorios = rel.sort((x, y) => {
+          //     return new Date(y.datadorelatorio).getTime() - new Date(x.datadorelatorio).getTime();
+          // });
+          this.relatorios = rel
+              .sort((a, b) => new Date(b.datadorelatorio).getTime() - new Date(a.datadorelatorio).getTime());
         })
   }
 
